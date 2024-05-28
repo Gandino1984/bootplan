a0551d709ff0b76ca5b0606ca61aa6e1132db8c4
Gandino1984 <andinogerman@gmail.com>
2024-05-28 11:10:11 +0200

    test button

--- 00-bootplan/src/main.jsx
+++ 00-bootplan/src/main.jsx
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const createButton = ({buttonText}) => {
+  return(
+    <button>{buttonText}</button>
+  );
+}
+
+
+root.render(
+  <>
+      {createButton({"buttonText":"botón"})}
+  </>
+);
