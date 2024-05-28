da3f087efc06c6d1e9a4da9b764e0560cab2a5d8
Gandino1984 <andinogerman@gmail.com>
2024-05-28 13:30:39 +0200

    fix UserView.jsx

--- 00-bootplan/src/main.jsx
+++ 00-bootplan/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import UserView from './assets/UserComponents/UserView/UserView.jsx'
 import './index.css'
-import './assets/UserComponents/UserView/UserView.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
