264e56f60e8c5fdde65688ef7bf07914a9f64798
Gandino1984 <andinogerman@gmail.com>
2024-05-28 12:44:55 +0200

    user view img & name add

--- 00-bootplan/src/assets/UserComponents/UserView/UserView.jsx
+++ 00-bootplan/src/assets/UserComponents/UserView/UserView.jsx
@@ -4,8 +4,13 @@ import './UserView.css'
 
 export function UserView() {
     return(
-        <article>
-            <h1>user name</h1>
+        <article className='user-view-article'>
+           <header className='user-view-header'>
+                <img className='user-view-avatar' src='https://unavatar.io/germanandino' alt="user avatar" />
+                <div className='user-view-userNameContainer'>
+                    <h1 className='user-view-userName'>German Andino</h1>
+                </div>
+           </header>
         </article>
     )
 }
--- 00-bootplan/src/main.jsx
+++ 00-bootplan/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import UserView from './assets/UserComponents/UserView/UserView.jsx'
 import './index.css'
+import './assets/UserComponents/UserView/UserView.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
