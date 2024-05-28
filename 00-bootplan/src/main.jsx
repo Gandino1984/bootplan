dedcb63b7a33300e8df3ef14b06555bf37ec1d82
Gandino1984 <andinogerman@gmail.com>
2024-05-28 11:45:37 +0200

    UserComponents folder creation

--- 00-bootplan/src/main.jsx
+++ 00-bootplan/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import UserView from './assets/UserView.jsx'
+import UserView from './assets/UserComponents/UserView.jsx'
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
