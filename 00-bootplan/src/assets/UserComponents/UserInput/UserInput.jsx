8f331ee57d64e9e771b348e237d98513983ac1db
Gandino1984 <andinogerman@gmail.com>
2024-05-28 20:49:26 +0200

    fix user register

--- 00-bootplan/src/assets/UserComponents/UserInput/UserInput.jsx
+++ 00-bootplan/src/assets/UserComponents/UserInput/UserInput.jsx
@@ -1,17 +1,48 @@
+import React, { useState } from 'react';
 
 import './UserInput.css'
 
-export function UserInput(){
-    
-    return(
-        <div className='user-input-formContainer'>
-            <form>
-                <input type="text" placeholder="nombre de usuario" />
-                <input type="text" placeholder="avatar" />
-                <input type="submit" value="CREAR" />
-            </form>
-        </div>
-    )
+
+export function UserInput() {
+  console.log("RENDER: UserInput");
+  
+  const [userName, setUserName] = useState('');
+  const [avatar, setAvatar] = useState('');
+
+  const handleUserNameChange = (event) => {
+    setUserName(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const handleAvatarChange = (event) => {
+    setAvatar(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Username:', userName, 'Avatar:', avatar);
+    // Aquí puedes agregar la lógica para manejar el envío del formulario
+  };
+
+  return (
+    <div className='user-input-formContainer'>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={userName}
+          onChange={handleUserNameChange}
+          placeholder="nombre de usuario"
+        />
+        <input
+          type="text"
+          value={avatar}
+          onChange={handleAvatarChange}
+          placeholder="avatar"
+        />
+        <input type="submit" value="CREAR" />
+      </form>
+    </div>
+  );
 }
 
 export default UserInput;
--- 00-bootplan/src/assets/UserComponents/UserRegister/UserRegister.jsx
+++ 00-bootplan/src/assets/UserComponents/UserRegister/UserRegister.jsx
@@ -1,13 +1,27 @@
+import React, { useState } from 'react';
+
 import UserInput from '../UserInput/UserInput.jsx';
+
 import UserView from '../UserView/UserView.jsx';
+
 import './UserRegister.css' 
 
 
 export function UserRegister(){
-    return(
+
+    const [userName, setUserName] = useState('');
+    const [userHandle, setUserHandle] = useState('');
+
+    const handleFormSubmit = (name, handle) => {
+        setUserName(name);
+        setUserHandle(handle);
+    };
+
+    return (
         <>
-            <UserInput />
-            <UserView />
+          <UserInput onFormSubmit={handleFormSubmit} />
+          <UserView userName={userName} userHandle={userHandle} />
         </>
-    )
+      );
+
 }
