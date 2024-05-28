1c1405c37fd1089dedef8b6031e4ccfdac614069
Gandino1984 <andinogerman@gmail.com>
2024-05-28 13:06:31 +0200

    user view css

--- 00-bootplan/src/assets/UserComponents/UserView/UserView.jsx
+++ 00-bootplan/src/assets/UserComponents/UserView/UserView.jsx
@@ -4,14 +4,15 @@ import './UserView.css'
 
 export function UserView() {
     return(
-        <article className='user-view-article'>
+        <div className='user-view-article'>
            <header className='user-view-header'>
                 <img className='user-view-avatar' src='https://unavatar.io/germanandino' alt="user avatar" />
                 <div className='user-view-userNameContainer'>
                     <h1 className='user-view-userName'>German Andino</h1>
+                    <h3 className='user-view-userHandle'>@germanandino</h3>
                 </div>
            </header>
-        </article>
+        </div>
     )
 }
 
