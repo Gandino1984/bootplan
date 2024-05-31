bd645efeca3a9a0ec20cf17cc38acae01d4bfc53
aitormon <aitormon@gmail.com>
2024-05-31 13:28:18 +0200

    barra 2.0

--- 00-bootplan/src/assets/ProyectComponents/BarraBusqueda/BarraBusqueda.jsx
+++ 00-bootplan/src/assets/ProyectComponents/BarraBusqueda/BarraBusqueda.jsx
@@ -43,7 +43,7 @@ const Header = () => {
         <input
           type="text"
           className="search-box"
-          placeholder="Buscar..."
+          placeholder="Buscar Proyecto"
           value={searchQuery}
           onChange={handleSearchChange}
         />
