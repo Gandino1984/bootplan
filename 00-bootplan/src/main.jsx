5bcf158afd3d365035c0d9653b8f9830d20dc663
Gandino1984 <andinogerman@gmail.com>
2024-06-01 19:35:43 +0200

    reinstall vite

--- 00-bootplan/src/main.jsx
+++ 00-bootplan/src/main.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './assets/App.jsx'
-import ComponentTestGerman from './assets/ComponentTestGerman.jsx';
-import ComponentTestLuis from './assets/ComponentTestLuis.jsx';
+import App from './App.jsx'
 
+import ProjectModal from './assets/ProyectComponents/projectModal/ProjectModal.jsx'
+import UserModal from './assets/UserComponents/UserInput/UserModal.jsx'
 import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-    <ComponentTestLuis />
-  </React.StrictMode>
-);
+    <UserModal />
+  </React.StrictMode>,
+)
