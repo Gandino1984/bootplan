214eaa6c9161d8c9923aed96ac929d33231f6023
Gandino1984 <andinogerman@gmail.com>
2024-06-01 08:44:10 +0200

    mainPAge

--- 00-bootplan/src/assets/ComponentTestGerman.jsx
+++ 00-bootplan/src/assets/ComponentTestGerman.jsx
@@ -4,11 +4,14 @@ import UserModal from "./UserComponents/UserInput/UserModal.jsx";
 
 import ProjectModal from "./ProyectComponents/projectModal/ProjectModal.jsx";
 
+ import MainProjectPage from "./mainProjectPage/MainProjectPage.jsx";
+
  export function ComponentTest(){
     return(
         <div className="ComponentTest-container">
             {/* <ProjectModal /> */}
-            <UserModal />
+            {/* <UserModal /> */}
+            <MainProjectPage />
         </div>   
     )
 }
